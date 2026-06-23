4b5e25fdb746c571846e144e6e2251d7eb77ea0c
Yadav Subba <yadavsubba2003@gmail.com>
2026-06-23 21:51:49 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,7 +6,6 @@ const firebaseConfig = {
   messagingSenderId: "535116640494",
   appId: "1:535116640494:web:4a02c00886fff2572503ff"
 };
-
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 
@@ -15,32 +14,26 @@ async function signupUser(email, password, displayName) {
   await cred.user.updateProfile({ displayName });
   return cred.user;
 }
-
 async function loginUser(email, password) {
   const cred = await auth.signInWithEmailAndPassword(email, password);
   return cred.user;
 }
-
 async function signInWithGoogle() {
   const provider = new firebase.auth.GoogleAuthProvider();
   const result = await auth.signInWithPopup(provider);
   return result.user;
 }
-
 async function logoutUser() {
   await auth.signOut();
   window.location.reload();
 }
-
 async function sendPasswordReset(email) {
   await auth.sendPasswordResetEmail(email);
 }
-
 function initAuthListener(callback) {
   auth.onAuthStateChanged(user => callback(user));
 }
 
-// Expose to global scope
 window.signupUser = signupUser;
 window.loginUser = loginUser;
 window.signInWithGoogle = signInWithGoogle;
